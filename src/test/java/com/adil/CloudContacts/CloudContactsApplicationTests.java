bc3ee2557f73e76e752319ef633f09243e858cab
adilxem <adil.shakeel104@gmail.com>
2024-10-11 16:07:03 +0530

    testing email service

--- src/test/java/com/adil/CloudContacts/CloudContactsApplicationTests.java
+++ src/test/java/com/adil/CloudContacts/CloudContactsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.adil.CloudContacts;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.adil.CloudContacts.Services.EmailService;
 
 @SpringBootTest
 class CloudContactsApplicationTests {
-
+	
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private EmailService emailService;
+
+	@Test
+	void sendEmailTest() {
+
+		emailService.sendEmail("23352004@pondiuni.ac.in", "Testing Email Service", "CloudContacts email verification link");
 	}
 
 }
